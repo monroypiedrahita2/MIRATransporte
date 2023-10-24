a50b693460cdcdf096edc472f65d4aa2bd6045e1
Juan Piedrahita <jmpiedra@bancolombia.com.co>
2023-10-24 18:46:38 -0500

    agregando un array de objetos

---
+++ MIRA-tu-transporte/src/app/shared/constants/puestos.const.ts
@@ -0,0 +1,283 @@
+export const PUESTOS_VOTACION = [
+  {
+    puesto: 'CASA DE LA CULTURA',
+    barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
+    direccion: 'CRA 16 # 49 30',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CASA-DE-LA-CULTURA',
+  },
+  {
+    puesto: 'CENTRO CULTURAL OTUN',
+    barrio: 'OTUN',
+    direccion: 'CRA 19 # 3 68',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-CULTURAL-OTUN',
+  },
+  {
+    puesto: 'CENTRO DOCENTE OTUN',
+    barrio: 'OTUN',
+    direccion: 'CRA 19 #3 71',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-DOCENTE-OTUN',
+  },
+  {
+    puesto: 'CENTRO EDUCATIVO LAS VIOLETAS',
+    barrio: 'LAS VIOLETAS',
+    direccion: 'Cll 26 Cr 28 esquina Los Lagos',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-EDUCATIVO-LAS-VIOLETAS',
+  },
+  {
+    puesto: 'COL FABIO VASQUEZ CAMPESTRE',
+    barrio: 'LA PRADERA',
+    direccion: 'CLL 23 # 27 15 LA PRADERA VIA NARANJALES',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COL-FABIO-VASQUEZ-CAMPESTRE',
+  },
+  {
+    puesto: 'COLEGIO BASICO SANTA JUANA',
+    barrio: 'LA PRADERA',
+    direccion: 'DIAG 21 TRANS 22',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-BASICO-SANTA-JUANA',
+  },
+  {
+    puesto: 'COLEGIO CRISTO REY',
+    barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
+    direccion: 'CALLE 67 # 18 01',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-CRISTO-REY',
+  },
+  {
+    puesto: 'COLEGIO EMPRESARIAL',
+    barrio: 'LA BADEA',
+    direccion: 'LA BADEA ANTIGUA PLAZA DE FERIAS',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-EMPRESARIAL',
+  },
+  {
+    puesto: 'COLEGIO HOGAR NAZARETH',
+    barrio: 'SANTA TERESITA',
+    direccion: 'CLL 59 # 14 32',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-HOGAR-NAZARETH',
+  },
+
+  {
+    puesto: 'CASA DE LA CULTURA',
+    barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
+    direccion: 'CRA 16 # 49 30',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CASA-DE-LA-CULTURA',
+  },
+  {
+    puesto: 'CENTRO CULTURAL OTUN',
+    barrio: 'OTUN',
+    direccion: 'CRA 19 # 3 68',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-CULTURAL-OTUN',
+  },
+  {
+    puesto: 'CENTRO DOCENTE OTUN',
+    barrio: 'OTUN',
+    direccion: 'CRA 19 #3 71',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-DOCENTE-OTUN',
+  },
+  {
+    puesto: 'CENTRO EDUCATIVO LAS VIOLETAS',
+    barrio: 'LAS VIOLETAS',
+    direccion: 'Cll 26 Cr 28 esquina Los Lagos',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-EDUCATIVO-LAS-VIOLETAS',
+  },
+  {
+    puesto: 'COL FABIO VASQUEZ CAMPESTRE',
+    barrio: 'LA PRADERA',
+    direccion: 'CLL 23 # 27 15 LA PRADERA VIA NARANJALES',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COL-FABIO-VASQUEZ-CAMPESTRE',
+  },
+  {
+    puesto: 'COLEGIO BASICO SANTA JUANA',
+    barrio: 'LA PRADERA',
+    direccion: 'DIAG 21 TRANS 22',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-BASICO-SANTA-JUANA',
+  },
+  {
+    puesto: 'COLEGIO CRISTO REY',
+    barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
+    direccion: 'CALLE 67 # 18 01',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-CRISTO-REY',
+  },
+  {
+    puesto: 'COLEGIO EMPRESARIAL',
+    barrio: 'LA BADEA',
+    direccion: 'LA BADEA ANTIGUA PLAZA DE FERIAS',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-EMPRESARIAL',
+  },
+  {
+    puesto: 'COLEGIO HOGAR NAZARETH',
+    barrio: 'SANTA TERESITA',
+    direccion: 'CLL 59 # 14 32',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-HOGAR-NAZARETH',
+  },
+  {
+    puesto: 'COLEGIO JUAN MANUEL GONZALEZ',
+    barrio: 'LOS NARANJOS',
+    direccion: 'CRA 11  CLL 46',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-JUAN-MANUEL-GONZALEZ',
+  },
+  {
+    puesto: 'COLEGIO MANUEL ELKIN PATARROYO',
+    barrio: 'CESAR AUGUSTO LOPEZ',
+    direccion: 'CLL 73 BIS CRA 21 BARRIO CESAR AUGUSTO LOPEZ',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-MANUEL-ELKIN-PATARROYO',
+  },
+  {
+    puesto: 'COLEGIO NUESTRA SRA DE GUADALUPE',
+    barrio: 'GUADALUPE',
+    direccion: 'CR 13A # 35 31',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUESTRA-SRA-DE-GUADALUPE',
+  },
+  {
+    puesto: 'COLEGIO NUEVA GRANADA',
+    barrio: 'San Diego',
+    direccion: 'CLL 51 CRA 22A',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUEVA-GRANADA',
+  },
+
+  {
+    puesto: 'COLEGIO NUEVA GRANADA',
+    barrio: 'San Diego',
+    direccion: 'CLL 51 CRA 22A',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUEVA-GRANADA',
+  },
+  {
+    puesto: 'COLEGIO NUESTRA SRA DE GUADALUPE SEDE 2',
+    barrio: 'GUADALUPE',
+    direccion: 'Cr 14 Cll 36 y 38',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUESTRA-SRA-DE-GUADALUPE-SEDE-2',
+  },
+  {
+    puesto: 'COLEGIO NUEVA GRANADA',
+    barrio: 'San Diego',
+    direccion: 'CLL 51 CRA 22A',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUEVA-GRANADA',
+  },
+  {
+    puesto: 'COLEGIO NUEVA REPUBLICA',
+    barrio: 'DESCONOCIDO',
+    direccion: 'MZ 17 ESQUINA',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUEVA-REPUBLICA',
+  },
+  {
+    puesto: 'COLEGIO PABLO VI',
+    barrio: 'LOS MOLINOS',
+    direccion: 'CL 44  # 23 24',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-PABLO-VI',
+  },
+  {
+    puesto: 'COLEGIO POPULAR DIOCESANO',
+    barrio: 'BADEA',
+    direccion: 'VIA TURIN LA POPA SECTOR LA BADEA',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-POPULAR-DIOCESANO',
+  },
+  {
+    puesto: 'COLEGIO RODOLFO LLINAS',
+    barrio: 'DESCONOCIDO',
+    direccion: 'Tv 10 #72D 09',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-RODOLFO-LLINAS',
+  },
+  {
+    puesto: 'COLEGIO SALESIANO',
+    barrio: 'DESCONOCIDO',
+    direccion: 'AV MOLINOS CL 29 FRENTE COMEST LA ROSA',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-SALESIANO',
+  },
+  {
+    puesto: 'COLEGIO VILLA FANNY',
+    barrio: 'JAPON',
+    direccion: 'CRA 17 # 8C 18',
+    ubicacion:'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-VILLA-FANNY',
+  },
+  {
+    puesto: "COLISEO ESTADIO DOSQUEBRADAS",
+    barrio: "VIA LA ROMELIA",
+    direccion: "KM 1 VIA ROMELIA EL POLLO",
+    ubicacion: "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLISEO-ESTADIO-DOSQUEBRADAS",
+  },
+  {
+    puesto: "CTRO DE RECLUSIÓN LA BADEA",
+    barrio: "LA BADEA",
+    direccion: "CTRO DE RECLUSIÓN LA BADEA",
+    ubicacion: "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CTRO-DE-RECLUSIÓN-LA-BADEA",
+  },
+  {
+    puesto: "CTRO DOCENTE CAMILO TORRES",
+    barrio: "CAMILO TORRES",
+    direccion: "PLAN 3 ZONA 6 CAMILO TORRES",
+    ubicacion: "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CTRO-DOCENTE-CAMILO-TORRES",
+  },
+  {
+    puesto: "ESCUELA LA ESNEDA",
+    barrio: "LA ESNEDA",
+    direccion: "Manzana 15 Casa 10",
+    ubicacion: "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-ESCUELA-LA-ESNEDA",
+  },
+  {
+    puesto: "ESCUELA SAN DIEGO",
+    barrio: "SAN DIEGO",
+    direccion: "CLL 56 # 19 58 SAN DIEGO",
+    ubicacion: "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-ESCUELA-SAN-DIEGO",
+  },
+  {
+    puesto: "I E AGUSTIN NIETO CABALLERO",
+    barrio: "FRAILES",
+    direccion: "CRA 43 # 15A  29 SANTIAGO LONDOÑO",
+    ubicacion: "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-I-E-AGUSTIN-NIETO-CABALLERO",
+  },
+  {
+    puesto: 'I E SANTA SOFIA',
+    barrio: 'JAPON',
+    direccion: 'CRA 24  #9 46',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-I-E-SANTA-SOFIA',
+  },
+  {
+    puesto: 'I E MARIA AUXILIADORA',
+    barrio: 'GUADALUPE',
+    direccion: 'CL 43 # 13 74',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-IE-MARIA-AUXILIADORA',
+  },
+  {
+    puesto: 'INST. EDUCATIVA BOSQUES DE LA ACUARELA',
+    barrio: 'BOSQUES DE LA ACUARELA',
+    direccion: 'CLL 70 TEJARES DE LA LOMA',
+    ubicacion:
+      'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-INST.-EDUCATIVA-BOSQUES-DE-LA-ACUARELA',
+  },
+  {
+    "puesto": "LAS MARCADAS ALTO DEL TORO",
+    "barrio": "DESCONOCIDO",
+    "direccion": "ESC. ALTO DEL TORO",
+    "ubicacion": "https://www.google.com/maps/search/Risaralda,-Dosquebradas,-LAS-MARCADAS-ALTO-DEL-TORO"
+  }
+];
