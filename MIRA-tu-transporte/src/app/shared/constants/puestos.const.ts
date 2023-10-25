8246b3044e7ec9ebde863f11593b942061fb2363
Juan Piedrahita <jmpiedra@bancolombia.com.co>
2023-10-24 19:28:09 -0500

    Se coloca id a array

--- MIRA-tu-transporte/src/app/shared/constants/puestos.const.ts
+++ MIRA-tu-transporte/src/app/shared/constants/puestos.const.ts
@@ -1,5 +1,6 @@
 export const PUESTOS_VOTACION = [
   {
+    id: 1,
     puesto: 'CASA DE LA CULTURA',
     barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
     direccion: 'CRA 16 # 49 30',
@@ -7,6 +8,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CASA-DE-LA-CULTURA',
   },
   {
+    id: 2,
     puesto: 'CENTRO CULTURAL OTUN',
     barrio: 'OTUN',
     direccion: 'CRA 19 # 3 68',
@@ -14,6 +16,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-CULTURAL-OTUN',
   },
   {
+    id: 3,
     puesto: 'CENTRO DOCENTE OTUN',
     barrio: 'OTUN',
     direccion: 'CRA 19 #3 71',
@@ -21,6 +24,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-DOCENTE-OTUN',
   },
   {
+    id: 4,
     puesto: 'CENTRO EDUCATIVO LAS VIOLETAS',
     barrio: 'LAS VIOLETAS',
     direccion: 'Cll 26 Cr 28 esquina Los Lagos',
@@ -28,6 +32,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-EDUCATIVO-LAS-VIOLETAS',
   },
   {
+    id: 4,
     puesto: 'COL FABIO VASQUEZ CAMPESTRE',
     barrio: 'LA PRADERA',
     direccion: 'CLL 23 # 27 15 LA PRADERA VIA NARANJALES',
@@ -35,6 +40,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COL-FABIO-VASQUEZ-CAMPESTRE',
   },
   {
+    id: 6,
     puesto: 'COLEGIO BASICO SANTA JUANA',
     barrio: 'LA PRADERA',
     direccion: 'DIAG 21 TRANS 22',
@@ -42,6 +48,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-BASICO-SANTA-JUANA',
   },
   {
+    id: 7,
     puesto: 'COLEGIO CRISTO REY',
     barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
     direccion: 'CALLE 67 # 18 01',
@@ -49,6 +56,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-CRISTO-REY',
   },
   {
+    id: 8,
     puesto: 'COLEGIO EMPRESARIAL',
     barrio: 'LA BADEA',
     direccion: 'LA BADEA ANTIGUA PLAZA DE FERIAS',
@@ -56,6 +64,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-EMPRESARIAL',
   },
   {
+    id: 9,
     puesto: 'COLEGIO HOGAR NAZARETH',
     barrio: 'SANTA TERESITA',
     direccion: 'CLL 59 # 14 32',
@@ -64,6 +73,7 @@ export const PUESTOS_VOTACION = [
   },
 
   {
+    id: 10,
     puesto: 'CASA DE LA CULTURA',
     barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
     direccion: 'CRA 16 # 49 30',
@@ -71,6 +81,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CASA-DE-LA-CULTURA',
   },
   {
+    id: 11,
     puesto: 'CENTRO CULTURAL OTUN',
     barrio: 'OTUN',
     direccion: 'CRA 19 # 3 68',
@@ -78,6 +89,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-CULTURAL-OTUN',
   },
   {
+    id: 12,
     puesto: 'CENTRO DOCENTE OTUN',
     barrio: 'OTUN',
     direccion: 'CRA 19 #3 71',
@@ -85,6 +97,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-DOCENTE-OTUN',
   },
   {
+    id: 13,
     puesto: 'CENTRO EDUCATIVO LAS VIOLETAS',
     barrio: 'LAS VIOLETAS',
     direccion: 'Cll 26 Cr 28 esquina Los Lagos',
@@ -92,6 +105,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CENTRO-EDUCATIVO-LAS-VIOLETAS',
   },
   {
+    id: 14,
     puesto: 'COL FABIO VASQUEZ CAMPESTRE',
     barrio: 'LA PRADERA',
     direccion: 'CLL 23 # 27 15 LA PRADERA VIA NARANJALES',
@@ -99,6 +113,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COL-FABIO-VASQUEZ-CAMPESTRE',
   },
   {
+    id: 15,
     puesto: 'COLEGIO BASICO SANTA JUANA',
     barrio: 'LA PRADERA',
     direccion: 'DIAG 21 TRANS 22',
@@ -106,6 +121,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-BASICO-SANTA-JUANA',
   },
   {
+    id: 16,
     puesto: 'COLEGIO CRISTO REY',
     barrio: 'AVENIDA SIMON BOLIVAR, CALLE 49',
     direccion: 'CALLE 67 # 18 01',
@@ -113,6 +129,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-CRISTO-REY',
   },
   {
+    id: 17,
     puesto: 'COLEGIO EMPRESARIAL',
     barrio: 'LA BADEA',
     direccion: 'LA BADEA ANTIGUA PLAZA DE FERIAS',
@@ -120,6 +137,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-EMPRESARIAL',
   },
   {
+    id: 18,
     puesto: 'COLEGIO HOGAR NAZARETH',
     barrio: 'SANTA TERESITA',
     direccion: 'CLL 59 # 14 32',
@@ -127,6 +145,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-HOGAR-NAZARETH',
   },
   {
+    id: 19,
     puesto: 'COLEGIO JUAN MANUEL GONZALEZ',
     barrio: 'LOS NARANJOS',
     direccion: 'CRA 11  CLL 46',
@@ -134,6 +153,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-JUAN-MANUEL-GONZALEZ',
   },
   {
+    id: 2,
     puesto: 'COLEGIO MANUEL ELKIN PATARROYO',
     barrio: 'CESAR AUGUSTO LOPEZ',
     direccion: 'CLL 73 BIS CRA 21 BARRIO CESAR AUGUSTO LOPEZ',
@@ -141,6 +161,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-MANUEL-ELKIN-PATARROYO',
   },
   {
+    id: 21,
     puesto: 'COLEGIO NUESTRA SRA DE GUADALUPE',
     barrio: 'GUADALUPE',
     direccion: 'CR 13A # 35 31',
@@ -148,6 +169,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUESTRA-SRA-DE-GUADALUPE',
   },
   {
+    id: 22,
     puesto: 'COLEGIO NUESTRA SRA DE GUADALUPE SEDE 2',
     barrio: 'GUADALUPE',
     direccion: 'Cr 14 Cll 36 y 38',
@@ -155,6 +177,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUESTRA-SRA-DE-GUADALUPE-SEDE-2',
   },
   {
+    id: 23,
     puesto: 'COLEGIO NUEVA GRANADA',
     barrio: 'San Diego',
     direccion: 'CLL 51 CRA 22A',
@@ -162,6 +185,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUEVA-GRANADA',
   },
   {
+    id: 23,
     puesto: 'COLEGIO NUEVA REPUBLICA',
     barrio: 'DESCONOCIDO',
     direccion: 'MZ 17 ESQUINA',
@@ -169,6 +193,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-NUEVA-REPUBLICA',
   },
   {
+    id: 25,
     puesto: 'COLEGIO PABLO VI',
     barrio: 'LOS MOLINOS',
     direccion: 'CL 44  # 23 24',
@@ -176,6 +201,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-PABLO-VI',
   },
   {
+    id: 26,
     puesto: 'COLEGIO POPULAR DIOCESANO',
     barrio: 'BADEA',
     direccion: 'VIA TURIN LA POPA SECTOR LA BADEA',
@@ -183,6 +209,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-POPULAR-DIOCESANO',
   },
   {
+    id: 27,
     puesto: 'COLEGIO RODOLFO LLINAS',
     barrio: 'DESCONOCIDO',
     direccion: 'Tv 10 #72D 09',
@@ -190,6 +217,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-RODOLFO-LLINAS',
   },
   {
+    id: 28,
     puesto: 'COLEGIO SALESIANO',
     barrio: 'DESCONOCIDO',
     direccion: 'AV MOLINOS CL 29 FRENTE COMEST LA ROSA',
@@ -197,6 +225,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-SALESIANO',
   },
   {
+    id: 29,
     puesto: 'COLEGIO VILLA FANNY',
     barrio: 'JAPON',
     direccion: 'CRA 17 # 8C 18',
@@ -204,6 +233,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLEGIO-VILLA-FANNY',
   },
   {
+    id: 30,
     puesto: 'COLISEO ESTADIO DOSQUEBRADAS',
     barrio: 'VIA LA ROMELIA',
     direccion: 'KM 1 VIA ROMELIA EL POLLO',
@@ -211,6 +241,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-COLISEO-ESTADIO-DOSQUEBRADAS',
   },
   {
+    id: 31,
     puesto: 'CTRO DE RECLUSIÓN LA BADEA',
     barrio: 'LA BADEA',
     direccion: 'CTRO DE RECLUSIÓN LA BADEA',
@@ -218,6 +249,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CTRO-DE-RECLUSIÓN-LA-BADEA',
   },
   {
+    id: 32,
     puesto: 'CTRO DOCENTE CAMILO TORRES',
     barrio: 'CAMILO TORRES',
     direccion: 'PLAN 3 ZONA 6 CAMILO TORRES',
@@ -225,6 +257,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-CTRO-DOCENTE-CAMILO-TORRES',
   },
   {
+    id: 33,
     puesto: 'ESCUELA LA ESNEDA',
     barrio: 'LA ESNEDA',
     direccion: 'Manzana 15 Casa 10',
@@ -232,6 +265,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-ESCUELA-LA-ESNEDA',
   },
   {
+    id: 34,
     puesto: 'ESCUELA POLICARPA SALAVARRIETA',
     barrio: 'SANTA ISABEL',
     direccion: 'CAMPESTRE C MZ 8',
@@ -239,6 +273,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-ESCUELA-POLICARPA-SALAVARRIETA',
   },
   {
+    id: 35,
     puesto: 'ESCUELA SAN DIEGO',
     barrio: 'SAN DIEGO',
     direccion: 'CLL 56 # 19 58 SAN DIEGO',
@@ -246,6 +281,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-ESCUELA-SAN-DIEGO',
   },
   {
+    id: 36,
     puesto: 'I E AGUSTIN NIETO CABALLERO',
     barrio: 'FRAILES',
     direccion: 'CRA 43 # 15A  29 SANTIAGO LONDOÑO',
@@ -253,6 +289,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-I-E-AGUSTIN-NIETO-CABALLERO',
   },
   {
+    id: 37,
     puesto: 'I E LA AURORA',
     barrio: 'LA AURORA',
     direccion: 'CRA 21 #11B  07',
@@ -260,6 +297,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-I-E-LA-AURORA',
   },
   {
+    id: 38,
     puesto: 'I E RENACIMIENTO',
     barrio: 'LA ROMELIA',
     direccion: 'CR 16 # 79 42 LA ROMELIA',
@@ -267,6 +305,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-I-E-RENACIMIENTO',
   },
   {
+    id: 39,
     puesto: 'I E SANTA SOFIA',
     barrio: 'JAPON',
     direccion: 'CRA 24  #9 46',
@@ -274,6 +313,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-I-E-SANTA-SOFIA',
   },
   {
+    id: 40,
     puesto: 'I E MARIA AUXILIADORA',
     barrio: 'GUADALUPE',
     direccion: 'CL 43 # 13 74',
@@ -281,6 +321,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-IE-MARIA-AUXILIADORA',
   },
   {
+    id: 41,
     puesto: 'IE SANTA ISABEL',
     barrio: 'SANTA ISABEL',
     direccion: 'DIAG 30 C # 5 59',
@@ -288,6 +329,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-IE-SANTA-ISABEL',
   },
   {
+    id: 42,
     puesto: 'INST. EDUCATIVA BOSQUES DE LA ACUARELA',
     barrio: 'BOSQUES DE LA ACUARELA',
     direccion: 'CLL 70 TEJARES DE LA LOMA',
@@ -295,6 +337,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-INST.-EDUCATIVA-BOSQUES-DE-LA-ACUARELA',
   },
   {
+    id: 43,
     puesto: 'INSTITUCION EDUCATIVA BOMBAY',
     barrio: 'BOMBAY',
     direccion: 'CARRERA 11 CALLE 65 Y 66 BOMBAY III',
@@ -302,6 +345,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-INSTITUCION-EDUCATIVA-BOMBAY',
   },
   {
+    id: 44,
     puesto: 'LAS MARCADAS ALTO DEL TORO',
     barrio: 'DESCONOCIDO',
     direccion: 'ESC. ALTO DEL TORO',
@@ -309,6 +353,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-LAS-MARCADAS-ALTO-DEL-TORO',
   },
   {
+    id: 45,
     puesto: 'LAS MARCADAS FRAILES',
     barrio: 'DESCONOCIDO',
     direccion: 'ESCUELA FRAILES',
@@ -316,6 +361,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-LAS-MARCADAS-FRAILES',
   },
   {
+    id: 46,
     puesto: 'MEGACOLEGIO BERNARDO LOPEZ PEREZ',
     barrio: 'CAMPRESTRE B',
     direccion: 'CRA 4 # 16 03',
@@ -323,6 +369,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-MEGACOLEGIO-BERNARDO-LOPEZ-PEREZ',
   },
   {
+    id: 47,
     puesto: 'PUESTO CENSO I E MANUELITA SAENZ',
     barrio: 'CAMPRESTE B',
     direccion: 'CRA 2 # 17B  13 CAMPESTRE B',
@@ -330,6 +377,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-PUESTO-CENSO-I-E-MANUELITA-SAENZ',
   },
   {
+    id: 48,
     puesto: 'SERRANIA ALTO DEL NUDO (AGUA AZUL)',
     barrio: 'DESCONOCIDO',
     direccion: 'VEREDA AGUA AZUL',
@@ -337,6 +385,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-SERRANIA-ALTO-DEL-NUDO-(AGUA-AZUL)',
   },
   {
+    id: 49,
     puesto: 'SERRANIA ALTO DEL NUDO (EL ESTANQUILLO)',
     barrio: 'DESCONOCIDO',
     direccion: 'VEREDA EL ESTANQUILLO',
@@ -344,6 +393,7 @@ export const PUESTOS_VOTACION = [
       'https://www.google.com/maps/search/Risaralda,-Dosquebradas,-SERRANIA-ALTO-DEL-NUDO-(EL-ESTANQUILLO)',
   },
   {
+    id: 50,
     puesto: 'SERRANIA ALTO NUDO LA UNION',
     barrio: 'ALTO DEL NUDO',
     direccion: 'LA UNION COLEGIO CARTAGENA',
